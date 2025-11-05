17a5e024ea6c2943297211a33cc87b76a02c7ac8
Dmitri Zagidulin <dzagidulin@gmail.com>
2025-11-04 23:15:13 -0500

    Update default app configs.
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- app.config.js
+++ app.config.js
@@ -1,8 +1,11 @@
 export const APP_URL =
-  'https://interop-alliance.github.io/wallet-to-webapp-demo';
+  'https://vcalm-request-demo.memetika.com';
+  // 'https://interop-alliance.github.io/wallet-to-webapp-demo';
 
 // Change this to your running exchanger URL
-export const EXCHANGER_SERVER_URL = 'https://federation-regarding-present-cingular.trycloudflare.com';
+export const EXCHANGER_SERVER_URL =
+  'https://minimal-exchanger.memetika.com';
+// 'https://federation-regarding-present-cingular.trycloudflare.com';
 
 export const WALLET_DEEP_LINK = 'https://lcw.app/request';
 export const CORS_PROXY = 'https://corsproxy.io/?';
